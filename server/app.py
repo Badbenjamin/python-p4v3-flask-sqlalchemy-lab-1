3447ef76af9668ecffad9c6b051d5ce10ddbf281
ben mccarthy <bmccarthy90@gmail.com>
2024-09-16 17:11:02 -0400

    tests pass

--- server/app.py
+++ server/app.py
@@ -21,7 +21,37 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def eq_by_id(id):
+    # remember to add .first or .all
+    eq = Earthquake.query.filter(Earthquake.id == id).first()
+    print(eq)
+    if eq is None:
+        return {"message" : f"Earthquake {id} not found."}, 404
+    else:
+        return eq.to_dict(), 200
+    
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def eqs_by_magnitude(magnitude):
+    eqs = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
 
+    result = []
+    for eq in eqs:
+        result.append(eq.to_dict())
+
+    return {
+        "count" : len(eqs),
+        "quakes" : result
+    }
+
+@app.route("/earthquakes/location/<string:location>")
+def eq_by_location(location):
+    eqs = Earthquake.query.filter(Earthquake.location == location).all()
+
+    result = []
+    for eq in eqs:
+        result.append(eq.to_dict())
+    return result
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,9 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+# serializer mixin has a todict method that creates a dictionary from its attributes
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData()
 
+
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+# needs to inherit from model
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = 'earthquakes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float)
+    location = db.Column(db.String)
+    year = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>'
